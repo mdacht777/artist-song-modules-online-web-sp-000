688d988626baad875bd85a62130a014c162f9057
Martin Achtermann <mdacht777@gmail.com>
2020-07-05 15:12:29 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,27 @@
-module Memorable
-  def reset_all
-    all.clear
-  end
+# module Memorable
+#   def reset_all
+#     all.clear
+#   end
 
-  def count
-    all.count
+#   def count
+#     all.count
+#   end
+# end
+
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
+  end
+ 
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 end
