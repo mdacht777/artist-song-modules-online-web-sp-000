cd2b655fa0131538c566d4e0fed31eda2039855d
Martin Achtermann <mdacht777@gmail.com>
2020-07-05 15:17:25 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,27 @@
+# module Memorable
+#   def reset_all
+#     all.clear
+#   end
+
+#   def count
+#     all.count
+#   end
+# end
+
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
+  end
+ 
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
